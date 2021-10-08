df1679076e7d7fb224f34892e5442fab33a041f2
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-10-08 14:13:35 +0530

    feat: get traverse to work

--- src/index.ts
+++ src/index.ts
@@ -8,42 +8,41 @@ const explorer = new MagicExplorer(
   path.basename(process.cwd()),
   []
 );
-const table = term.DataTable(
-  {
-    x: 0,
-    y: 5,
-    width: null,
-    height: term.height,
-    style: term.brightWhite.bgBlack,
-    selectedStyle: term.bgBrightWhite.black,
-    scrollPadding: 3,
-    padding: 1,
-    filterTextSize: 16,
-    columns: [
-      {
-        get(content) {
-          return content.name;
-        },
-        width: 20,
-        style(item) {
-          return item.isDir ? term.brightMagenta : term.cyan;
-        },
+const tableConfig = {
+  x: 0,
+  y: 5,
+  width: null,
+  height: term.height,
+  style: term.brightWhite.bgBlack,
+  selectedStyle: term.bgBrightWhite.black,
+  scrollPadding: 3,
+  padding: 1,
+  filterTextSize: 16,
+  columns: [
+    {
+      get(content) {
+        return content.name;
       },
-    ],
-  },
-  () => {
-    console.log("Called");
-  }
-);
-
-table.setData(explorer.getChildren());
-table.promise.then((item) => {
-  console.log(item);
+      width: 20,
+      style(item) {
+        return item.isDir ? term.brightMagenta : term.cyan;
+      },
+    },
+  ],
+};
+const submitCallback = (item) => {
   const res = explorer.commitAction({
     name: item.name.trim(),
     verb: "display",
   });
   if (res.redraw) {
-    table.setData(explorer.getChildren());
+    term
+      .DataTable(tableConfig)
+      .setData(explorer.getChildren()) //modified the node dependency to allow this chaining
+      .promise.then(submitCallback);
   }
-});
+};
+
+const table = term.DataTable(tableConfig);
+table.setData(explorer.getChildren());
+table.promise.then(submitCallback);
