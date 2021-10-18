3c072c2c081522f9d19b5eb30e3ccf4375528bee
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-10-18 08:57:20 +0530

    refactor: give better file names

---
+++ src/flagParser.ts
@@ -0,0 +1,23 @@
+import { argv } from "process";
+import { configDescriptor, FlagTypes } from "./types";
+export default function (): configDescriptor[] {
+  let arg: string[] = argv.slice(2);
+  const flagTypes: configDescriptor[] = [];
+  for (let i = 0; i < arg.length - 1; i++) {
+    const flag = arg[i],
+      val = String(arg[i + 1]);
+    if (flag == "-E") {
+      flagTypes[FlagTypes.FilterExtension] = {
+        flag: "filterExtensions",
+        value: val,
+      };
+    } else if (flag === "-G") {
+      flagTypes[FlagTypes.Gitignore] = { flag: "gitignore", value: "" };
+    } else if (flag === "-B") {
+      flagTypes[FlagTypes.Before] = { flag: "before", value: val };
+    } else if (flag === "-A") {
+      flagTypes[FlagTypes.After] = { flag: "after", value: val };
+    }
+  }
+  return flagTypes;
+}
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type contentDescriptor = {
+  name: string;
+  isDir: Boolean;
+  size: string;
+  lastModified: string;
+  meta: string;
+  toPath: string;
+  fullPath?: string;
+};
+export type configDescriptor = {
+  flag: "filterExtensions" | "after" | "before" | "gitignore";
+  value: string;
+};
+export interface config {
+  ignores: string[];
+}
+export enum FlagTypes {
+  FilterExtension = 0,
+  Gitignore = 1,
+  Before = 2,
+  After = 3,
+}
