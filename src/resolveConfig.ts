f45cef9d899d9295db2185ceb8719894fe9a48e0
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-10-18 08:56:57 +0530

    feat: resolve config

---
+++ src/resolveConfig.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs";
+import * as path from "path";
+import { config } from "./types";
+const dotFileLocation = path.resolve(
+  process.env[process.platform === "win32" ? "USERPROFILE" : "HOME"],
+  ".zeus"
+);
+let options: config = {
+  ignores: [],
+};
+if (!fs.existsSync(dotFileLocation)) {
+  const defaults = JSON.stringify(options, null, 2);
+  fs.writeFileSync(dotFileLocation, defaults);
+} else {
+  const dotFileConfig = fs.readFileSync(dotFileLocation, {
+    encoding: "utf8",
+    flag: "r",
+  });
+  if (dotFileConfig.trim() === "") {
+    const defaults = JSON.stringify(options, null, 2);
+    fs.writeFileSync(dotFileLocation, defaults);
+  } else {
+    const parsedConfig = JSON.parse(dotFileConfig);
+    options.ignores = parsedConfig.ignores || [];
+  }
+}
+export default options;
