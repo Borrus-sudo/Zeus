795007ffa972a9d768e97862b26953451a00d6ef
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-11-04 20:06:31 +0530

    fix: fix ls mode renderer bugs

--- src/CustomRenderer.ts
+++ src/CustomRenderer.ts
@@ -8,20 +8,25 @@ export default function (data: contentDescriptor[], term) {
   const green = "^g";
   const yellow = "^y";
   const padding = " ".repeat(1);
-  for (let content of data) {
+  for (let content of data.slice(1)) {
     const metaContent = `${blue}${content.meta.slice(0, 10).padEnd(10, " ")}`;
     const lastModified = `${red}${content.lastModified
       .slice(0, 19)
       .padEnd(19, " ")}`;
     const size = `${green}${content.size.slice(0, 9).padEnd(9, " ")}`;
-    const indicator = `${yellow}${content.isDir ? "<DIR>" : "<FILE>"}`;
+    const indicator = `${yellow}${(content.isDir ? "<DIR>" : "<FILE>").padEnd(
+      8,
+      " "
+    )}`;
     const dirent = `${(FlagList[FlagTypes.Icons]
       ? appendGlyph(content.name, content.isDir)
       : content.name
     )
       .slice(0, 30)
       .padEnd(30, " ")}`;
-    const out = `${bold}${padding}${metaContent}${padding}${size}${padding}${lastModified}${padding}${indicator}${padding}${dirent}`;
+    const out = `${bold}${metaContent}${padding}${size}${padding}${lastModified}${padding}${indicator}${padding}${
+      content.isDir ? "^c" : "^w"
+    }${dirent}`;
     term(out + "\n");
   }
 }
