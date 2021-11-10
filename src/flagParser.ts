0f80649794778e4f478829b3c81cf75238f3d91e
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-11-10 23:24:50 +0530

    feat: add help flag

--- src/flagParser.ts
+++ src/flagParser.ts
@@ -4,7 +4,7 @@ import { flagDescriptor, FlagTypes } from "./types";
 function getFlags(): flagDescriptor[] {
   let arg: string[] = argv.slice(2);
   const flagTypes: flagDescriptor[] = [];
-  
+
   for (let i = 0; i < arg.length; i++) {
     const flag = arg[i];
     let val: string;
@@ -21,7 +21,7 @@ function getFlags(): flagDescriptor[] {
           value: val,
         };
         break;
-      
+
       case "-B":
         val = String(arg[i + 1]);
         if (val === "undefined") {
@@ -31,7 +31,7 @@ function getFlags(): flagDescriptor[] {
         i++;
         flagTypes[FlagTypes.Before] = { flag: "before", value: val };
         break;
-      
+
       case "-A":
         val = String(arg[i + 1]);
         if (val === "undefined") {
@@ -41,7 +41,7 @@ function getFlags(): flagDescriptor[] {
         i++;
         flagTypes[FlagTypes.After] = { flag: "after", value: val };
         break;
-      
+
       case "-R":
         val = String(arg[i + 1]);
         if (val === "undefined") {
@@ -51,7 +51,7 @@ function getFlags(): flagDescriptor[] {
         i++;
         flagTypes[FlagTypes.Regex] = { flag: "regex", value: val };
         break;
-      
+
       case "-fd":
         val = String(arg[i + 1]);
         if (val === "undefined") {
@@ -61,7 +61,7 @@ function getFlags(): flagDescriptor[] {
         i++;
         flagTypes[FlagTypes.Find] = { flag: "find", value: val };
         break;
-      
+
       case "-gi":
         val = String(arg[i + 1]);
         if (val === "undefined") {
@@ -69,7 +69,7 @@ function getFlags(): flagDescriptor[] {
           process.exit();
         }
         i++;
-        
+
         flagTypes[FlagTypes.GIgnore] = {
           flag: "globalIgnore",
           value: val
@@ -80,7 +80,7 @@ function getFlags(): flagDescriptor[] {
             .join(","),
         };
         break;
-      
+
       case "-qi":
         val = String(arg[i + 1]);
         if (val === "undefined") {
@@ -88,7 +88,7 @@ function getFlags(): flagDescriptor[] {
           process.exit();
         }
         i++;
-       
+
         flagTypes[FlagTypes.QIgnore] = {
           flag: "queryIgnore",
           value: val
@@ -99,13 +99,28 @@ function getFlags(): flagDescriptor[] {
             .join(","),
         };
         break;
-      
+
       case "--icons":
         flagTypes[FlagTypes.Icons] = { flag: "icons", value: val };
         break;
-      
+
       case "--ls":
         flagTypes[FlagTypes.LS] = { flag: "ls", value: val };
+        break;
+
+      case "--help":
+        const heading = String.raw`
+ ________   _______  __    __       _______.
+|       /  |   ____||  |  |  |     /       |
+ ---/  /   |  |__   |  |  |  |    |    (----
+   /  /    |   __|  |  |  |  |     \   \    
+  /  /----.|  |____ |  ---   | .----)   |   
+ /________||_______| \______/  |_______/    
+`;
+        console.log(heading + "\n");
+        const flagInfo = `-fd flag, pass a glob pattern to this flag to display all the files matching the glob pattern \n-B flag, pass a date with this flag to display all files created before the given date\n-A flag, pass a date with this flag to display all files created after the given date\n-P flag, pass a label with this flag to display all the folders classifying as the label or folders containing these such folders.\n--ls flag, pass this to start Zeus in a non-interactive mode\n--icons flag, pass this to get icons based on your file extensions, the icons are customizable via the config file`;
+        console.log(flagInfo);
+        process.exit();
     }
   }
   return flagTypes;
